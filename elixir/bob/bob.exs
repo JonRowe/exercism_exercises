dd494b99e8553bdaaf992d29711e93fb05eb99ba
Jon Rowe <hello@jonrowe.co.uk>
2013-08-04 08:27:49 +1000

    refactor to module

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -2,12 +2,24 @@ defmodule Teenager do
 
   def hey(""),                         do: "Fine. Be that way!"
 
-  def hey(text) do
+  def hey(message) do
     cond do
-      String.ends_with?(text, "?") -> "Sure."
-      String.upcase(text) == text  -> "Woah, chill out!"
-      true                         -> "Whatever."
+      Language.is_question?(message)  -> "Sure."
+      Language.is_aggresive?(message) -> "Woah, chill out!"
+      true                            -> "Whatever."
     end
+  end
+
+end
+
+defmodule Language do
+
+  def is_aggresive?(message) do
+    String.upcase(message) == message
+  end
+
+  def is_question?(message) do
+    String.ends_with?(message, "?")
   end
 
 end
