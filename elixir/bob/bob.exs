7b4f10566f45b3a6f8d4b8f4680374f723d10be1
Jon Rowe <hello@jonrowe.co.uk>
2013-08-03 21:13:25 +1000

    use string match directly

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -4,18 +4,14 @@ defmodule Teenager do
 
   def hey(text) do
     cond do
-      perceived_aggresive(text) -> "Woah, chill out!"
-      asked_question(text)      -> "Sure."
+      String.ends_with?(text, "?") -> "Sure."
+      perceived_aggresive(text)    -> "Woah, chill out!"
       true                         -> "Whatever."
     end
   end
 
   def perceived_aggresive(text) do
     Regex.match?(%r/^[^a-z]+$/, text)
-  end
-
-  def asked_question(text) do
-    Regex.match?(%r/\?$/, text)
   end
 
 end
