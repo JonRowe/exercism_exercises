0f22faac0f1572a6319fd977db0288045afd5e61
Jon Rowe <hello@jonrowe.co.uk>
2013-08-03 20:01:32 +1000

    1st draft solution

---
+++ elixir/bob/bob.exs
@@ -0,0 +1,27 @@
+defmodule Teenager do
+
+  def hey(text) when text == "" do
+    "Fine. Be that way!"
+  end
+
+  def hey(text) do
+    cond do
+      perceived_aggresive(text) -> "Woah, chill out!"
+      asked_question(text)      -> "Sure."
+      otherwise(text)           -> "Whatever."
+    end
+  end
+
+  def perceived_aggresive(text) do
+    Regex.match?(%r/^[^a-z]+$/, text)
+  end
+
+  def asked_question(text) do
+    Regex.match?(%r/\?$/, text)
+  end
+
+  def otherwise(text) do
+    true
+  end
+
+end
