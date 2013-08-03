28da898dbc2240143a52d22c67fd45bb6dedf0bc
Jon Rowe <hello@jonrowe.co.uk>
2013-08-03 21:16:11 +1000

    use uppercase comparison to check for all caps

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -5,13 +5,9 @@ defmodule Teenager do
   def hey(text) do
     cond do
       String.ends_with?(text, "?") -> "Sure."
-      perceived_aggresive(text)    -> "Woah, chill out!"
+      String.upcase(text) == text  -> "Woah, chill out!"
       true                         -> "Whatever."
     end
-  end
-
-  def perceived_aggresive(text) do
-    Regex.match?(%r/^[^a-z]+$/, text)
   end
 
 end
