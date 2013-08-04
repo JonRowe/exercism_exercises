ad3e39625bae05c0f20e7fe514a8c899b542e191
Jon Rowe <hello@jonrowe.co.uk>
2013-08-04 16:38:20 +1000

    refactor with language module

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,13 +1,31 @@
 class Bob
 
   def hey message
-    case (message || "")
-      when ""             then "Fine. Be that way!"
-      when message.upcase then "Woah, chill out!"
-      when /\?$/          then "Sure."
+    message.extend(Language)
+
+    case
+      when message.is_blank?     then "Fine. Be that way!"
+      when message.is_aggresive? then "Woah, chill out!"
+      when message.is_question?  then "Sure."
     else
       "Whatever."
     end
+  end
+
+end
+
+module Language
+
+  def is_aggresive?
+    upcase == self
+  end
+
+  def is_question?
+    end_with? '?'
+  end
+
+  def is_blank?
+    nil == self || "" == self
   end
 
 end
