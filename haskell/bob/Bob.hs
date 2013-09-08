710a174e8b6442a0c4fc43770785f35f105eb070
Jon Rowe <hello@jonrowe.co.uk>
2013-09-08 20:16:08 +1000

    [haskell] refactor to a datatype

--- haskell/bob/Bob.hs
+++ haskell/bob/Bob.hs
@@ -1,12 +1,23 @@
 module Bob where
   import Data.Char
 
+  data EmotionalContent = Silence | Aggression | Question | Meh
+
+  instance Show EmotionalContent where
+    show Silence = "Fine. Be that way!"
+    show Aggression = "Woah, chill out!"
+    show Question = "Sure."
+    show Meh = "Whatever."
+
   responseFor :: String -> String
-  responseFor message
-    | isSilence    message = "Fine. Be that way!"
-    | isAggressive message = "Woah, chill out!"
-    | isQuestion   message = "Sure."
-    | otherwise            = "Whatever."
+  responseFor message = show (classify message)
+
+  classify :: String -> EmotionalContent
+  classify message
+    | isSilence    message = Silence
+    | isAggressive message = Aggression
+    | isQuestion   message = Question
+    | otherwise            = Meh
 
   isAggressive :: String -> Bool
   isAggressive message = any isUpper message && all (not . isLower) message
