60e1889a592f4e4c05431436f026c230af6e8eb7
Jon Rowe <hello@jonrowe.co.uk>
2013-09-11 19:44:31 +1000

    [elixir] refactor to privated methods

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -1,14 +1,18 @@
 defmodule Words do
 
   def count(words) do
-    count_fnx = fn (word, dict) ->
-      HashDict.update dict, word, 1, fn (count) -> count + 1 end
-    end
-    List.foldl list(words), HashDict.new, count_fnx
+    List.foldl list(words), HashDict.new, update_count
   end
 
-  def list(words) do
-    Regex.scan %r/\w+/, String.downcase(words)
+  defp list(words) do
+    List.flatten Regex.scan %r/\w+/, String.downcase(words)
   end
+
+  defp update_count do
+    fn (word, dict) ->
+      HashDict.update dict, word, 1, fn (count) -> count + 1 end
+    end
+  end
+
 
 end
