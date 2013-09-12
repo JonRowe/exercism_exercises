fd493d362dd4511722d74e2178634360bc3aa45b
Jon Rowe <hello@jonrowe.co.uk>
2013-09-12 21:57:47 +1000

    [elixir] remove anonymous function

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -1,17 +1,15 @@
 defmodule Words do
 
   def count(words) do
-    List.foldl list(words), HashDict.new, update_count
+    List.foldl list(words), HashDict.new, update_count(&1, &2)
   end
 
   defp list(words) do
     List.flatten Regex.scan %r/\w+/, String.downcase(words)
   end
 
-  defp update_count do
-    fn (word, dict) ->
-      HashDict.update dict, word, 1, fn (count) -> count + 1 end
-    end
+  defp update_count(word, dict) do
+    HashDict.update dict, word, 1, fn (count) -> count + 1 end
   end
 
 
