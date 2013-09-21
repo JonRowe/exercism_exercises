f26905afc4e494bfcaf9f0b0c3f32d13929b589b
Jon Rowe <hello@jonrowe.co.uk>
2013-09-21 21:42:02 +1000

    [elixir] move word matching to seperate function and rename anagram?

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -1,12 +1,16 @@
 defmodule Anagram do
 
   def match(word, potential_anagrams) do
-    Enum.filter potential_anagrams, &(is_anagram? String.downcase(word), String.downcase(&1))
+    Enum.filter potential_anagrams, &(anagram? String.downcase(word), String.downcase(&1))
   end
 
-  defp is_anagram?(word, potential) do
-    word != potential &&
+  defp anagram?(word, potential) do
+    not_same?(word, potential) &&
     contains_all_chars?( String.codepoints(potential), String.codepoints(word) )
+  end
+
+  defp not_same?(a, b) do
+    a != b
   end
 
   defp contains_all_chars?(list_a, list_b) do
