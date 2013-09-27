4f77e0ea12ae4c15335c3efe3756e7bde861cb57
Jon Rowe <hello@jonrowe.co.uk>
2013-09-27 10:36:15 +1000

    [elixir] inline string downcasing into pipeline and rename word matcher method

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -1,15 +1,15 @@
 defmodule Anagram do
 
   def match(word, potential_anagrams) do
-    Enum.filter potential_anagrams, &(anagram? String.downcase(word), String.downcase(&1))
+    Enum.filter potential_anagrams, &(anagram? word, &1)
   end
 
   defp anagram?(word, potential) do
-    not_same?(word, potential) && contains_all_chars?(word, potential)
+    are_not_identical?(word, potential) && contains_all_chars?(word, potential)
   end
 
-  defp not_same?(a, b) do
-    a != b
+  defp are_not_identical?(word_a, word_b) do
+    String.downcase(word_a) != String.downcase(word_b)
   end
 
   defp contains_all_chars?(word_a, word_b) do
@@ -18,6 +18,8 @@ defmodule Anagram do
 
   defp chars(word) do
     word
+    |>
+    String.downcase
     |>
     String.codepoints
     |>
