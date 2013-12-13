7028ce7cd70bc1bf00fc8dd900a4d2a9d5de2926
Jon Rowe <hello@jonrowe.co.uk>
2013-12-13 11:59:35 +1100

    refactor to use better module names

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,12 +1,24 @@
 module Hamming
   module_function
 
-  def compute strand_a, strand_b
+  def compute dna_strand_a, dna_strand_b
     diff = 0
-    strand_a.split('').each_with_index do |code, index|
-      diff += 1 if strand_b[index] && code != strand_b[index]
+    each_base_pair(dna_strand_a, dna_strand_b) do |a, b|
+      diff += 1 if b && a != b
     end
     diff
   end
+
+  def each_base_pair dna_strand_a, dna_strand_b
+    split(dna_strand_a).each_with_index do |a, index|
+      yield a, dna_strand_b[index]
+    end
+  end
+  private :each_base_pair
+
+  def split strand
+    strand.split('')
+  end
+  private :split
 
 end
