3e397c725a471b96f3ab3514375e18306615a3f0
Jon Rowe <hello@jonrowe.co.uk>
2013-12-25 19:47:29 +1100

    alternate hamming using zip and count

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,19 +2,15 @@ module Hamming
   module_function
 
   def compute dna_strand_a, dna_strand_b
-    diff = 0
-    each_base_pair(dna_strand_a, dna_strand_b) do |a, b|
-      diff += 1 if b && a != b
+    combine_base_pairs(dna_strand_a, dna_strand_b).count do |(a, b)|
+      b && a != b
     end
-    diff
   end
 
-  def each_base_pair dna_strand_a, dna_strand_b
-    split(dna_strand_a).each_with_index do |a, index|
-      yield a, dna_strand_b[index]
-    end
+  def combine_base_pairs dna_strand_a, dna_strand_b
+    split(dna_strand_a).zip split(dna_strand_b)
   end
-  private :each_base_pair
+  private :combine_base_pairs
 
   def split strand
     strand.split('')
