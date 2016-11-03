01b45cc107ff45d1cf371ec482c8e0160f6ccc8f
Jon Rowe <hello@jonrowe.co.uk>
2016-11-03 11:06:57 +1100

    update word-count exercise to latest

---
+++ elixir/word-count/word_count.exs
@@ -0,0 +1,19 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map
+  def count(sentence) do
+    List.foldl list(sentence), %{}, &update_count(&1, &2)
+  end
+
+  defp list(words) do
+    List.flatten Regex.scan ~r/\w+/, String.downcase(words)
+  end
+
+  defp update_count(word, dict) do
+    Map.update dict, word, 1, fn (count) -> count + 1 end
+  end
+end
